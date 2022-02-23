7faa5a96810e2e4bebed0429c3458416a6f68ade
ROBOJessie2019 <57692031+R080Jessica20@users.noreply.github.com>
2022-02-22 19:17:03 -0600

    Add files via upload

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,7 +16,45 @@ public final class Constants {
     public static final int SHOOTER_TELEOP = 1;
 
     public static final double SHOOTER_TELEOP_SPEED = 1.0;
+    public static final double ElevatorSpeed=0.6;
+    public static final double Telescopic_Arm_Speed=0.8;
+    //Buttons
+    public static final int ELEVATOR_UP_BUTTON = 5;
+    public static final int ELEVATOR_DOWN_BUTTON = 3;
 
+    //-----------------MOTORS-------------
     public static final int SHOOTER_MOTOR_RIGHT = 2;
     public static final int SHOOTER_MOTOR_LEFT = 3;
+
+    public static final int ELEVATOR_LEFT_MOTOR=13;
+    public static final int ELEVATOR_RIGHT_MOTOR=2;
+
+    public static final int TELESCOPIC_ARM_LEFT_MOTOR=4;
+    public static final int TELESCOPIC_ARM_RIGHT_MOTOR=7;
+
+    public static final int TELESCOPIC_ARM_UP_MOTOR=9;
+    public static final int TELESCOPIC_ARM_DOWN_MOTOR=0;
+    
+
+    //-----------------PORTS-------------
+    //Elevator
+    public static final int ELEVATOR_LEFT_PORT1=0;
+    public static final int ELEVATOR_LEFT_PORT2=1;
+
+    public static final int ELEVATOR_RIGHT_PORT1=6;
+    public static final int ELEVATOR_RIGHT_PORT2=7;
+
+    public static final int ELEVATOR_LIMIT_LEFT=9;
+    public static final int ELEVATOR_LIMIT_RIGHT=8;
+
+    //Telescopic Arm
+    //random ports
+    public static final int TELESCOPIC_ARM_LEFT_PORT1=8;
+    public static final int TELESCOPIC_ARM_LEFT_PORT2=6;
+    public static final int TELESCOPIC_ARM_RIGHT_PORT1=2;
+    public static final int TELESCOPIC_ARM_RIGHT_PORT2=5;
+
+    public static final int TELESCOPIC_ARM_LIMIT_LEFT=3;
+    public static final int TELESCOPIC_ARM_LIMIT_RIGHT=1;
+
 }

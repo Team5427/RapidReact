41cf16b22a5404781ed153280c8ac20aa4ffb2ba
ROBOJessie2019 <57692031+R080Jessica20@users.noreply.github.com>
2022-02-22 19:17:31 -0600

    Add files via upload

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,11 +5,15 @@
 package frc.robot;
 
 
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
@@ -17,9 +21,12 @@ import edu.wpi.first.wpilibj2.command.button.Button;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 //import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj2.command.Command;
-
+import frc.robot.commands.MoveElevator;
 import frc.robot.commands.MoveShooterTeleop;
+import frc.robot.commands.MoveTelescopicArm;
+import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.TelescopicArm;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -31,26 +38,69 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private static Joystick joy;
   private static Button shooterTeleop;
+  private static Button MoveElevatorUp;
+  private static Button MoveElevatorDown;
+  private static Button MoveTelescopicArmUp;
+  private static Button MoveTelescopicArmDown;
+  private static Button MoveTelescopicArmRight;
+  private static Button MoveTelescopicArmLeft;
+  
+
 
   //motors 
   public static CANSparkMax shooterMotorRight;
   public static CANSparkMax shooterMotorLeft;
+  public static  WPI_VictorSPX elevatorMotorLeft;
+  public static  WPI_VictorSPX elevatorMotorRight;
 
   //sensors
   private static RelativeEncoder shooterRightEnc;
   private static RelativeEncoder shooterLeftEnc;
+  private static Encoder elevatorRightEnc;
+  private static Encoder elevatorLeftEnc;
 
   //subsystems
   private static Shooter shooter;
-
+  private static Elevator elevator;
+  private static TelescopicArm telescopicArm;
   private static SparkMaxPIDController pidcontrol_shooter_Right;
   private static SparkMaxPIDController pidcontrol_shooter_Left;
+
+//elevator & TeleOps
+private static WPI_VictorSPX elevatorLeft;
+private static WPI_VictorSPX elevatorRight;
+private static DigitalInput leftLimitSwitch;
+private static DigitalInput rightLimitSwitch;
+
+private static WPI_VictorSPX telescopicArmLeft;
+private static WPI_VictorSPX telescopicArmRight;
+private static Encoder telescopicArmRightEnc;
+private static Encoder telescopicArmLefttEnc;
+private static DigitalInput leftLimitSwitchTele;
+private static DigitalInput rightLimitSwitchTele;
 
   public static PowerDistribution pdp;
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+//elevator
+    elevatorLeft= new   WPI_VictorSPX(Constants.ELEVATOR_LEFT_MOTOR);
+    elevatorRight = new   WPI_VictorSPX(Constants.ELEVATOR_RIGHT_MOTOR);
+    elevatorLeftEnc=  new Encoder(Constants.ELEVATOR_LEFT_PORT1, Constants.ELEVATOR_LEFT_PORT2);
+    elevatorRightEnc=  new Encoder(Constants.ELEVATOR_LEFT_PORT1, Constants.ELEVATOR_LEFT_PORT2);
+    leftLimitSwitch= new DigitalInput(Constants.ELEVATOR_LIMIT_LEFT);
+    rightLimitSwitch= new DigitalInput(Constants.ELEVATOR_LIMIT_RIGHT);
+    elevator= new Elevator(elevatorLeft, elevatorRight, leftLimitSwitch, rightLimitSwitch, elevatorLeftEnc, elevatorRightEnc);
+    //telescopic Arm
+    telescopicArmLeft= new   WPI_VictorSPX(Constants.TELESCOPIC_ARM_LEFT_MOTOR);
+    telescopicArmRight = new   WPI_VictorSPX(Constants.TELESCOPIC_ARM_RIGHT_MOTOR);
+    leftLimitSwitchTele= new DigitalInput(Constants.TELESCOPIC_ARM_LIMIT_LEFT);
+    rightLimitSwitchTele= new DigitalInput(Constants.TELESCOPIC_ARM_LIMIT_RIGHT);
+    telescopicArmRightEnc= new Encoder(Constants.TELESCOPIC_ARM_RIGHT_PORT1, Constants.TELESCOPIC_ARM_RIGHT_PORT2);
+    telescopicArmLefttEnc= new Encoder(Constants.TELESCOPIC_ARM_LEFT_PORT1,Constants.TELESCOPIC_ARM_LEFT_PORT2);
+    telescopicArm= new TelescopicArm(telescopicArmLeft, telescopicArmRight, leftLimitSwitchTele, rightLimitSwitchTele, telescopicArmLefttEnc, telescopicArmRightEnc);
+
     shooterMotorRight = new CANSparkMax(Constants.SHOOTER_MOTOR_RIGHT, MotorType.kBrushless);
-  shooterMotorLeft = new CANSparkMax(Constants.SHOOTER_MOTOR_LEFT, MotorType.kBrushless);
+   shooterMotorLeft = new CANSparkMax(Constants.SHOOTER_MOTOR_LEFT, MotorType.kBrushless);
 
   pidcontrol_shooter_Right = shooterMotorRight.getPIDController();
   pidcontrol_shooter_Left = shooterMotorLeft.getPIDController();
@@ -60,7 +110,6 @@ public class RobotContainer {
   shooterLeftEnc = shooterMotorLeft.getEncoder();
 
   pdp = new PowerDistribution(0, ModuleType.kCTRE);
-
 
   
 
@@ -80,13 +129,31 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
-  
+  private void configureButtonBindings() {
+    joy = new Joystick(0);
+    MoveElevatorUp = new JoystickButton(joy, Constants.ELEVATOR_UP_BUTTON);
+    MoveElevatorDown = new JoystickButton(joy, Constants.ELEVATOR_DOWN_BUTTON);
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
    */
+  //shooter
+
+  //Elevator
+  MoveElevatorUp.whileHeld(new MoveElevator(Constants.ElevatorSpeed));
+  MoveElevatorDown.whileHeld(new MoveElevator(-Constants.ElevatorSpeed));
+
+  //Up and Down
+  MoveTelescopicArmUp.whileHeld(new MoveElevator(Constants.Telescopic_Arm_Speed));
+  MoveTelescopicArmDown.whileHeld(new MoveElevator(-Constants.Telescopic_Arm_Speed));
+
+  //Left and Right
+  MoveTelescopicArmRight.whileHeld(new MoveElevator(Constants.Telescopic_Arm_Speed));
+  MoveTelescopicArmLeft.whileHeld(new MoveElevator(-Constants.Telescopic_Arm_Speed));
+
+
+  }
   public static Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
     return null;
@@ -94,4 +161,7 @@ public class RobotContainer {
 
   public static Shooter getShooter(){return shooter;}
   public static Joystick getJoy(){return joy;}
+  public static Elevator getElevator(){return elevator;}
+  public static TelescopicArm getTelescopicArm(){return telescopicArm; }
+
 }

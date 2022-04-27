d8efdb400d1fdcc9995b7b74b3e14646db4b3fdd
VictoriaZ04 <vzhang0779@gmail.com>
2022-04-26 21:01:05 -0500

    keyboard mouse

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,14 +1,18 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase{
 
     private MotorControllerGroup left, right;
     private DifferentialDrive drive;
+    private SlewRateLimiter xLimiter = new SlewRateLimiter(.10);
+    private SlewRateLimiter zLimiter = new SlewRateLimiter(.10);
 
     public DriveTrain(MotorControllerGroup left, MotorControllerGroup right, DifferentialDrive drive){
         this.left = left;
@@ -26,7 +30,7 @@ public class DriveTrain extends SubsystemBase{
 
     public void driveWithJoystick(Joystick joy){
 
-        drive.arcadeDrive(-joy.getY()*.95555666, joy.getZ() * .55);
+        drive.arcadeDrive(xLimiter.calculate(SmartDashboard.getEntry("/SmartDashboard/x").getDouble(0)), zLimiter.calculate(SmartDashboard.getEntry("/SmartDashboard/z").getDouble(0)));
     }
 
     public void stop(){
